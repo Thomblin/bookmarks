b99f7c48ce9a9126ca8d94bb2bcac25a5d363d7a
Sebastian Detert <detert@billiger-mietwagen.de>
2015-01-07 10:37:48 +0100

    improved example file. filter input variables

--- public/index.php
+++ public/index.php
@@ -6,6 +6,14 @@ use Bookmarks\Storage;
 use Bookmarks\View;
 use Bookmarks\Controller;
 
+$server  = filter_input_array(INPUT_SERVER, FILTER_SANITIZE_STRING);
+$get     = filter_input_array(INPUT_GET, FILTER_SANITIZE_STRING);
+$post    = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+$request = array_merge(
+    is_null($get) ? array() : $get,
+    is_null($post) ? array() : $post
+);
+
 $env = new \Bookmarks\Environment();
 $env->inject('subdomain', 'dev'); // replace all {subdomain} parts with dev
 
@@ -14,4 +22,4 @@ $view       = new View('../cache');
 $controller = new Controller($storage, $view);
 $controller->injectEnvironment($env);
 
-echo $controller->parseRequest($_SERVER['REQUEST_METHOD'], $_REQUEST);
+echo $controller->parseRequest($server['REQUEST_METHOD'], $request);
